f19e5948efd675fcc75aa0589c57f6a483ed598e
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-02-14 16:52:50 +0000

    inital set up code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Flashcards from "./flashcards";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Flashcards />
+    </div>
+  );
+}
+
+export default App;
